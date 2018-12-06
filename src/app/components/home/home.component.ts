19412cdbca490e63bd0401944290fbf8d2afdbda
Barnixx <rb.kuszaj@gmail.com>
2018-12-06 19:21:26 +0100

    Add server-sent event to CategoryService as Observable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {CategoryService} from "../../services/category/category.service";
+import {Subscription} from "rxjs";
+import {CategoryModel} from "../../models/category.model";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  private sseStream: Subscription;
+  categories: Array<CategoryModel> = [];
+
+  constructor(private categoryService: CategoryService) {
   }
 
   ngOnInit() {
+    this.categoryService.getSseCategory().subscribe(stream => {
+      console.log(stream);
+      this.categories = stream;
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.sseStream) {
+      this.sseStream.unsubscribe();
+    }
   }
 
 }
--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -18,13 +18,26 @@ const httpHeader = {
 })
 export class CategoryService {
 
-  public categories: Array<PagedResponseModel>;
-
-
   constructor(private http: HttpClient) { }
 
   getCategory(root: boolean): Observable<PagedResponseModel<CategoryModel>> {
     let params = new HttpParams().set('root', root.toString());
     return this.http.get<PagedResponseModel<CategoryModel>>(categoryApiUrl, {params: params});
+  }
+
+  getSseCategory(): Observable<Array<CategoryModel>> {
+    return new Observable<Array<CategoryModel>>(observer => {
+      const source: EventSource = new EventSource(categoryApiUrl + '/stream');
+      source.onmessage = x => {
+        let jsonString: any = JSON.parse(x.data);
+        let categories: Array<CategoryModel> = <Array<CategoryModel>>jsonString;
+        observer.next(categories);
+      };
+      source.onerror = x => console.log(x);
+
+      return () => {
+        source.close();
+      };
+    });
   }
 }
